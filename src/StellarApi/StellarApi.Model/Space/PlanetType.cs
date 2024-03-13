397e313e1385d9dd51cbd444a4f5f4993c314ed0
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-03-13 18:45:21 +0100

    [#2] Add PlanetType class

---
+++ src/StellarApi/StellarApi.Model/Space/PlanetType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarApi.Model.Space
+{
+    /// <summary>
+    /// Represents the type of a planet.
+    /// </summary>
+    public enum PlanetType
+    {
+        /// <summary>
+        /// Terrestrial planets are rocky planets with a solid surface.
+        /// </summary>
+        Terrestrial,
+
+        /// <summary>
+        /// Gas giants are large planets primarily composed of gases.
+        /// </summary>
+        Gas
+    }
+
+}

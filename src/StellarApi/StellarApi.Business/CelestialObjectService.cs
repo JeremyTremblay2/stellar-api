35e0baab98d7a87b8b682559998a83110416a23b
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-03-25 20:09:36 +0100

    [#7] Add services

---
+++ src/StellarApi/StellarApi.Business/CelestialObjectService.cs
@@ -0,0 +1,46 @@
+﻿using StellarApi.Infrastructure.Business;
+using StellarApi.Infrastructure.Repository;
+using StellarApi.Model.Space;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarApi.Business
+{
+    public class CelestialObjectService : ICelestialObjectService
+    {
+        private readonly ICelestialObjectRepository _repository;
+
+        public CelestialObjectService(ICelestialObjectRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<bool> DeleteCelestialObject(int id)
+        {
+            return _repository.RemoveCelestialObject(id);
+        }
+
+        public Task<CelestialObject?> GetCelestialObject(int id)
+        {
+            return _repository.GetCelestialObject(id);
+        }
+
+        public Task<IEnumerable<CelestialObject>> GetCelestialObjects(int page, int pageSize)
+        {
+            return _repository.GetCelestialObjects(page, pageSize);
+        }
+
+        public Task<bool> PostCelestialObject(CelestialObject celestialObject)
+        {
+            return _repository.AddCelestialObject(celestialObject);
+        }
+
+        public Task<bool> PutCelestialObject(int id, CelestialObject celestialObject)
+        {
+            return _repository.EditCelestialObject(id, celestialObject);
+        }
+    }
+}

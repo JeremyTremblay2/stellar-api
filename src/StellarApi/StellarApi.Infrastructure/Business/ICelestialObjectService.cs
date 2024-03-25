720ad39324ac234888547879a08ace74a223a939
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-03-25 20:07:58 +0100

    [#7] Add infrastructure

---
+++ src/StellarApi/StellarApi.Infrastructure/Business/ICelestialObjectService.cs
@@ -0,0 +1,22 @@
+﻿using StellarApi.Model.Space;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarApi.Infrastructure.Business
+{
+    public interface ICelestialObjectService
+    {
+        Task<CelestialObject?> GetCelestialObject(int id);
+
+        Task<IEnumerable<CelestialObject>> GetCelestialObjects(int page, int pageSize);
+
+        Task<bool> PostCelestialObject(CelestialObject celestialObject);
+
+        Task<bool> PutCelestialObject(int id, CelestialObject celestialObject);
+
+        Task<bool> DeleteCelestialObject(int id);
+    }
+}
---
+++ src/StellarApi/StellarApi.Infrastructure/Repository/ICelestialObjectRepository.cs
@@ -0,0 +1,22 @@
+﻿using StellarApi.Model.Space;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellarApi.Infrastructure.Repository
+{
+    public interface ICelestialObjectRepository
+    {
+        Task<CelestialObject?> GetCelestialObject(int id);
+
+        Task<IEnumerable<CelestialObject>> GetCelestialObjects(int page, int pageSize);
+
+        Task<bool> AddCelestialObject(CelestialObject celestialObject);
+
+        Task<bool> EditCelestialObject(int id, CelestialObject celestialObject);
+
+        Task<bool> RemoveCelestialObject(int id);
+    }
+}

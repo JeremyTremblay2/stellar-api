729df688b31eadec51c4bfa95ef80e8293bbbca4
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-03-30 11:35:57 +0100

    [#21] Add some documentation.

--- src/StellarApi/StellarApi.DTOtoModel/CelestialObjectExtensions.cs
+++ src/StellarApi/StellarApi.DTOtoModel/CelestialObjectExtensions.cs
@@ -4,8 +4,16 @@ using StellarApi.Model.Space;
 
 namespace StellarApi.DTOtoModel
 {
+    /// <summary>
+    /// Extension methods for converting between CelestialObject and CelestialObjectDTO.
+    /// </summary>
     public static class CelestialObjectExtensions
     {
+        /// <summary>
+        /// Converts a CelestialObjectDTO to a CelestialObject.
+        /// </summary>
+        /// <param name="dto">The CelestialObjectDTO to convert.</param>
+        /// <returns>The converted CelestialObject.</returns>
         public static CelestialObject ToModel(this CelestialObjectDTO dto)
         {
             if (dto.Type.Equals("Star"))
@@ -19,7 +27,7 @@ namespace StellarApi.DTOtoModel
                 var isWater = GetPropertyValue<bool>(dto.Metadata, "isWater");
                 var isLife = GetPropertyValue<bool>(dto.Metadata, "isLife");
                 var planetType = GetPropertyValue<PlanetType>(dto.Metadata, "planetType");
-                return new Planet(dto.Id, dto.Name, dto.Description, dto.Image, dto.Mass, dto.Temperature, dto.Radius, 
+                return new Planet(dto.Id, dto.Name, dto.Description, dto.Image, dto.Mass, dto.Temperature, dto.Radius,
                     isWater ?? false, isLife ?? false, planetType ?? PlanetType.Undefined);
             }
             else
@@ -28,11 +36,21 @@ namespace StellarApi.DTOtoModel
             }
         }
 
+        /// <summary>
+        /// Converts a collection of CelestialObjectDTOs to CelestialObjects.
+        /// </summary>
+        /// <param name="dtos">The collection of CelestialObjectDTOs to convert.</param>
+        /// <returns>The converted collection of CelestialObjects.</returns>
         public static IEnumerable<CelestialObject> ToModel(this IEnumerable<CelestialObjectDTO> dtos)
         {
             return dtos.Select(dto => dto.ToModel()).Where(obj => obj != null).Select(obj => obj!);
         }
 
+        /// <summary>
+        /// Converts a CelestialObject to a CelestialObjectDTO.
+        /// </summary>
+        /// <param name="model">The CelestialObject to convert.</param>
+        /// <returns>The converted CelestialObjectDTO.</returns>
         public static CelestialObjectDTO ToDTO(this CelestialObject model)
         {
             var dto = new CelestialObjectDTO
@@ -52,11 +70,11 @@ namespace StellarApi.DTOtoModel
             {
                 dto.Type = "Planet";
                 dto.Metadata = new List<PropertyDTO>
-                {
-                    new PropertyDTO { Name = "isWater", Type = "bool", Value = planet.IsWater.ToString() },
-                    new PropertyDTO { Name = "isLife", Type = "bool", Value = planet.IsLife.ToString() },
-                    new PropertyDTO { Name = "planetType", Type = "PlanetType", Value = planet.PlanetType.ToString() }
-                };
+            {
+                new PropertyDTO { Name = "isWater", Type = "bool", Value = planet.IsWater.ToString() },
+                new PropertyDTO { Name = "isLife", Type = "bool", Value = planet.IsLife.ToString() },
+                new PropertyDTO { Name = "planetType", Type = "PlanetType", Value = planet.PlanetType.ToString() }
+            };
             }
             else
             {
@@ -67,11 +85,23 @@ namespace StellarApi.DTOtoModel
             return dto;
         }
 
+        /// <summary>
+        /// Converts a collection of CelestialObjects to CelestialObjectDTOs.
+        /// </summary>
+        /// <param name="models">The collection of CelestialObjects to convert.</param>
+        /// <returns>The converted collection of CelestialObjectDTOs.</returns>
         public static IEnumerable<CelestialObjectDTO> ToDTO(this IEnumerable<CelestialObject> models)
         {
             return models.Select(model => model.ToDTO()).Where(obj => obj != null).Select(obj => obj!);
         }
 
+        /// <summary>
+        /// Gets the value of a property from a collection of PropertyDTOs.
+        /// </summary>
+        /// <typeparam name="T">The type of the property value.</typeparam>
+        /// <param name="properties">The collection of PropertyDTOs.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <returns>The value of the property if found; otherwise, null.</returns>
         private static T? GetPropertyValue<T>(IEnumerable<PropertyDTO> properties, string propertyName) where T : struct
         {
             foreach (var prop in properties)
@@ -90,6 +120,14 @@ namespace StellarApi.DTOtoModel
             }
             return null;
         }
+
+        /// <summary>
+        /// Tries to parse the value to the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to parse to.</typeparam>
+        /// <param name="value">The value to parse.</param>
+        /// <param name="result">When this method returns, contains the parsed value if the conversion succeeded, or the default value if the conversion failed.</param>
+        /// <returns>True if the value was parsed successfully; otherwise, false.</returns>
         private static bool TryParseValue<T>(string value, out T result) where T : struct
         {
             result = default(T);

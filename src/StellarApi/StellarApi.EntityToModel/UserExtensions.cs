a4761c478bda287479c9b211961b544b912eb980
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-04-27 18:04:24 +0200

    [#30] Add extensions methods for the users

---
+++ src/StellarApi/StellarApi.EntityToModel/UserExtensions.cs
@@ -0,0 +1,51 @@
+﻿using StellarApi.Model.Users;
+using StellarApi.Entities;
+
+namespace StellarApi.EntityToModel
+{
+    /// <summary>
+    /// Extension methods for converting between <see cref="User"/> and <see cref="UserEntity"/>.
+    /// </summary>
+    public static class UserExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="UserEntity"/> to a <see cref="User"/>.
+        /// </summary>
+        /// <param name="entity">The Entity to convert.</param>
+        /// <returns>The new User object.</returns>
+        public static User ToModel(this UserEntity entity)
+            => new User(entity.Id, entity.Email, entity.Username, entity.Password, entity.CreationDate, entity.ModificationDate);
+
+        /// <summary>
+        /// Converts a <see cref="User"/> to a <see cref="UserEntity"/>.
+        /// </summary>
+        /// <param name="model">The model to convert.</param>
+        /// <returns>The new entity User object.</returns>
+        public static UserEntity ToEntity(this User model)
+            => new UserEntity
+            {
+                Id = model.Id,
+                Email = model.Email,
+                Username = model.Username,
+                Password = model.Password,
+                CreationDate = model.CreationDate,
+                ModificationDate = model.ModificationDate
+            };
+
+        /// <summary>
+        /// Converts a collection of <see cref="UserEntity"/> to a collection of <see cref="User"/>.
+        /// </summary>
+        /// <param name="entities">The entities to be converted.</param> 
+        /// <returns>A new list of User objects.</returns>
+        public static IEnumerable<User> ToModel(this IEnumerable<UserEntity> entities)
+            => entities.Select(entity => entity.ToModel());
+
+        /// <summary>
+        /// Converts a collection of <see cref="User"/> to a collection of <see cref="UserEntity"/>.
+        /// </summary>
+        /// <param name="models">The User objects to be converted.</param>
+        /// <returns>A new list of UserEntity object.</returns>
+        public static IEnumerable<UserEntity> ToEntity(this IEnumerable<User> models)
+            => models.Select(model => model.ToEntity());
+    }
+}

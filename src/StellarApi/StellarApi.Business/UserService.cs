d593028654be52276a3a7259d40b87bb34c8ae42
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-04-27 18:37:42 +0200

    [#30] Add user service

---
+++ src/StellarApi/StellarApi.Business/UserService.cs
@@ -0,0 +1,50 @@
+﻿using StellarApi.Infrastructure.Business;
+using StellarApi.Infrastructure.Repository;
+using StellarApi.Model.Users;
+
+namespace StellarApi.Business
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserService"/> class.
+        /// </summary>
+        /// <param name="userRepository">The repository used by this service.</param>
+        public UserService(IUserRepository userRepository)
+        {
+            this._repository = userRepository;
+        }
+
+        /// <inheritdoc/>
+        public async Task<User> GetUserById(int id)
+        {
+            return await _repository.GetUserById(id);
+        }
+
+        /// <inheritdoc/>
+        public async Task<IEnumerable<User>> GetUsers(int page, int pageSize)
+        {
+            return await _repository.GetUsers(page, pageSize);
+        }
+
+        /// <inheritdoc/>
+        public async Task<bool> PostUser(User user)
+        {
+            return await _repository.AddUser(user);
+        }
+
+        /// <inheritdoc/>
+        public async Task<bool> PutUser(User user)
+        {
+            return await _repository.EditUser(user);
+        }
+
+        /// <inheritdoc/>
+        public async Task<bool> DeleteUser(int id)
+        {
+            return await _repository.RemoveUser(id);
+        }
+    }
+}

44b983a3bf60bf01e2694b790eba070a94f1e62f
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-05-24 00:24:32 +0200

    [#43] Add tests to check the codecov CI

---
+++ src/StellarApi/StellarApi.ApiUnitTests/CelestialObjectRepositoryTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using StellarApi.Entities;
+using StellarApi.Model.Space;
+using StellarApi.Repository.Context;
+using StellarApi.Repository.Repositories;
+
+namespace StellarApi.ApiUnitTests
+{
+    [TestClass]
+    public class CelestialObjectRepositoryTest
+    {
+        [TestMethod]
+        public async Task AddCelestialObject_ShouldReturnTrue_WhenSuccessful()
+        {
+            // Arrange
+            var celestialObject = new Star(1, "Sun", "The Sun is the star at the center of the Solar System.", "image",
+                new Model.Geometry.Position(0, 0, 0), 1.989 * Math.Pow(10, 30), 5778, 696340, 70000, StarType.YellowDwarf, DateTime.Now, DateTime.Now);
+            var mockSet = new Mock<DbSet<CelestialObjectEntity>>();
+            var mockContext = new Mock<SpaceDbContext>();
+
+            var repository = new CelestialObjectRepository();
+
+            // Act
+            /*var result = await repository.AddCelestialObject(celestialObject);
+
+            // Assert
+            Assert.IsTrue(result);
+            mockSet.Verify(m => m.AddAsync(It.IsAny<CelestialObjectEntity>(), It.IsAny<CancellationToken>()), Times.Once());
+            mockContext.Verify(m => m.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once());*/
+        }
+    }
+}

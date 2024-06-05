d6eb038d9d9e15ef324db7c584feb8112265af5d
Jérémy Tremblay <jeremy-tremblay@outlook.fr>
2024-06-05 23:26:42 +0200

    [#55] Add swagger doc for schemas

--- src/StellarApi/StellarApi.DTOs/Auth/ApiTokenRequest.cs
+++ src/StellarApi/StellarApi.DTOs/Auth/ApiTokenRequest.cs
@@ -1,18 +1,23 @@
-﻿namespace StellarApi.RestApi.Auth
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace StellarApi.RestApi.Auth
 {
     /// <summary>
     /// Represents the request object for an API token.
     /// </summary>
+    [SwaggerSchema("The request object to get a new set of API tokens.", ReadOnly = true)]
     public class ApiTokenRequest
     {
         /// <summary>
         /// Gets or sets the access token.
         /// </summary>
-        public required string AccessToken { get; set; }
+        [SwaggerSchema(Description = "The access token of the user (expire or not).", Nullable = false)]
+        public string AccessToken { get; set; }
 
         /// <summary>
         /// Gets or sets the refresh token.
         /// </summary>
+        [SwaggerSchema(Description = "The refresh token of the user (should not be expired).", Nullable = false)]
         public required string RefreshToken { get; set; }
     }
 }
--- src/StellarApi/StellarApi.DTOs/Auth/LoginRequest.cs
+++ src/StellarApi/StellarApi.DTOs/Auth/LoginRequest.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace StellarApi.RestApi.Auth
 {
     /// <summary>
     /// Represents a login request.
     /// </summary>
+    [SwaggerSchema("The login request made by a user to connect him to the application.", ReadOnly = true)]
     public class LoginRequest
     {
         /// <summary>
@@ -12,12 +14,14 @@ namespace StellarApi.RestApi.Auth
         /// </summary>
         [Required]
         [EmailAddress]
+        [SwaggerSchema(Description = "The email adress of the user.", Nullable = false)]
         public string Email { get; set; }
 
         /// <summary>
         /// Gets or sets the password.
         /// </summary>
         [Required]
+        [SwaggerSchema(Description = "The encrypted password of the user.", Nullable = false)]
         public string Password { get; set; }
     }
 }
